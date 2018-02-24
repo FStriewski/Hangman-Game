82783f0b12380d2cfd3d305bed85e4922dc42688
FStriewski <fstriewski@outlook.com>
2018-02-24 11:46:51 +0100

    Add game container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import GameContainer from './containers/gameContainer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        A test
+        <GameContainer />
       </div>
     );
   }
---
+++ src/containers/gameContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './gameContainer.css';
+
+class GameContainer extends Component {
+  render() {
+    return (
+      <div className="GameContainer">
+        This is a container
+      </div>
+    );
+  }
+}
+
+export default GameContainer;
