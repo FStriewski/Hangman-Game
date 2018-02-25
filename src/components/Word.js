89f05369cb98c342343650a8afd77019bf1ab3b9
FStriewski <fstriewski@outlook.com>
2018-02-25 13:24:05 +0100

    Change Word to receive the correct props

--- src/components/Word.js
+++ src/components/Word.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import './Word.css';
 import { connect } from 'react-redux'
-import {wordToGuess, wordHidden} from './wordRepos'
+//import {wordToGuess, wordHidden} from './wordRepos'
 
 
 class Word extends Component {
@@ -10,12 +10,12 @@ class Word extends Component {
   render() {
     return (
       <div className="Word">
-      {wordToGuess}<br/>
-      {wordHidden}
+      {this.props.replace.wordToGuess}<br/>
+      {this.props.replace.wordHidden}
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ word }) => ({ word })
+const mapStateToProps = ({ replace }) => ({ replace })
 export default connect(mapStateToProps, {})(Word)
